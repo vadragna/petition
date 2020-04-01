6485c396e3bbb2c96ea2a6483fec5bdad7b0c654
Valerio Adragna <valerioadragna89@hotmail.it>
2020-04-01 17:52:00 +0200

    some logs deleted

--- db.js
+++ db.js
@@ -4,134 +4,132 @@ var app = express();
 const spicePg = require("spiced-pg");
 
 const db = spicePg(
-    process.env.DATABASE_URL ||
-        "postgres://postgres:postgres@localhost:5432/petition"
+  process.env.DATABASE_URL ||
+    "postgres://postgres:postgres@localhost:5432/petition"
 );
 
 app.use(express.static("./static"));
 
 exports.addSigner = function(sig, user_id) {
-    return db.query(
-        `INSERT INTO signatures (sig, user_id)
+  return db.query(
+    `INSERT INTO signatures (sig, user_id)
     VALUES ($1, $2) returning id`,
-        [sig, user_id]
-    );
+    [sig, user_id]
+  );
 };
 
 exports.getSigners = function() {
-    return db.query(`SELECT * FROM users
+  return db.query(`SELECT * FROM users
     LEFT JOIN user_profiles
     ON user_id = users.id`);
 };
 
 exports.getSignerFromCity = function(city) {
-    let lowerCity = city.toLowerCase();
-    let capitalizedCity = city[0].toUpperCase() + city.slice(1, 10);
-    console.log("capitalizedCity", capitalizedCity);
-    console.log(lowerCity, "lowerCity");
-    {
-        return db.query(
-            `SELECT * FROM users
+  let lowerCity = city.toLowerCase();
+  let capitalizedCity = city[0].toUpperCase() + city.slice(1, 10);
+  {
+    return db.query(
+      `SELECT * FROM users
         LEFT JOIN user_profiles
         ON user_id = users.id
         WHERE city=$1
         OR city=$2
         OR city=$3`,
-            [lowerCity, city, capitalizedCity]
-        );
-    }
+      [lowerCity, city, capitalizedCity]
+    );
+  }
 };
 
 exports.addUser = function(first, last, email, password) {
-    return db.query(
-        `INSERT INTO users (first, last, email, password)
+  return db.query(
+    `INSERT INTO users (first, last, email, password)
 VALUES ($1, $2, $3, $4) RETURNING id`,
-        [first, last, email, password]
-    );
+    [first, last, email, password]
+  );
 };
 
 exports.newProfile = function(age, city, hp, userId) {
-    return db.query(
-        `INSERT INTO user_profiles (age, city, url, user_id)
+  return db.query(
+    `INSERT INTO user_profiles (age, city, url, user_id)
         VALUES ($1, $2, $3, $4) returning id`,
-        [age, city, hp, userId]
-    );
+    [age, city, hp, userId]
+  );
 };
 
 exports.getDataFromEmail = function(email) {
-    return db.query(
-        `SELECT * FROM users
+  return db.query(
+    `SELECT * FROM users
         LEFT JOIN signatures
         ON user_id = users.id
         WHERE email=$1`,
-        [email]
-    );
+    [email]
+  );
 };
 
 exports.getPassword = function(email) {
-    console.log("email in db.js", email);
-    return db.query(`SELECT * FROM users WHERE email=$1`, [email]);
+  console.log("email in db.js", email);
+  return db.query(`SELECT * FROM users WHERE email=$1`, [email]);
 };
 
 exports.getUserId = function(email) {
-    return db.query(`SELECT id FROM users WHERE email=$1`, [email]);
+  return db.query(`SELECT id FROM users WHERE email=$1`, [email]);
 };
 
 exports.getSigImg = function(user_id) {
-    return db.query(`SELECT * FROM users
+  return db.query(`SELECT * FROM users
         LEFT JOIN signatures
         ON user_id = users.id
         WHERE user_id='${user_id}'`);
 };
 
 exports.getAllUserData = function(userId) {
-    return db.query(
-        `SELECT * FROM users
+  return db.query(
+    `SELECT * FROM users
         LEFT JOIN user_profiles
         ON user_id = users.id
         WHERE user_id=$1`,
-        [userId]
-    );
+    [userId]
+  );
 };
 
 exports.updateUsersTable = function(userId, first, last, email) {
-    return db.query(
-        `UPDATE users
+  return db.query(
+    `UPDATE users
      SET first =$2, last=$3, email=$4
      WHERE id=$1`,
-        [userId, first, last, email]
-    );
+    [userId, first, last, email]
+  );
 };
 
 exports.updateUsersTableWithPw = function(
-    userId,
-    first,
-    last,
-    email,
-    password
+  userId,
+  first,
+  last,
+  email,
+  password
 ) {
-    return db.query(
-        `UPDATE users
+  return db.query(
+    `UPDATE users
      SET first =$2, last=$3, email=$4, password=$5
      WHERE id=$1`,
-        [userId, first, last, email, password]
-    );
+    [userId, first, last, email, password]
+  );
 };
 
 exports.updateProfile = function(age, city, url, userId) {
-    return db.query(
-        `INSERT INTO user_profiles (age, city, url, user_id)
+  return db.query(
+    `INSERT INTO user_profiles (age, city, url, user_id)
         VALUES ($1, $2, $3, $4)
         ON CONFLICT (user_id) DO
         UPDATE SET age = $1, city = $2, url = $3`,
-        [age || null, city || null, url || null, userId]
-    );
+    [age || null, city || null, url || null, userId]
+  );
 };
 
 exports.deleteSig = function(userId) {
-    return db.query(
-        `DELETE from signatures
+  return db.query(
+    `DELETE from signatures
         WHERE user_id=$1`,
-        [userId]
-    );
+    [userId]
+  );
 };
--- index.js
+++ index.js
@@ -3,24 +3,24 @@ var app = express();
 exports.app = app;
 
 const {
-    requireSignature,
-    requireNoSignature,
-    requireLoggedOutUser
+  requireSignature,
+  requireNoSignature,
+  requireLoggedOutUser
 } = require("./middleware");
 
 var {
-    addSigner,
-    getSigners,
-    addUser,
-    getSigImg,
-    newProfile,
-    getSignerFromCity,
-    getAllUserData,
-    updateProfile,
-    getDataFromEmail,
-    updateUsersTable,
-    updateUsersTableWithPw,
-    deleteSig
+  addSigner,
+  getSigners,
+  addUser,
+  getSigImg,
+  newProfile,
+  getSignerFromCity,
+  getAllUserData,
+  updateProfile,
+  getDataFromEmail,
+  updateUsersTable,
+  updateUsersTableWithPw,
+  deleteSig
 } = require("./db.js");
 const { hash, compare } = require("./utils/bc.js");
 
@@ -33,272 +33,253 @@ app.engine("handlebars", hb());
 app.set("view engine", "handlebars");
 
 app.use(
-    express.urlencoded({
-        extendend: false
-    })
+  express.urlencoded({
+    extendend: false
+  })
 );
 
 app.use(express.static("./public"));
 
 app.use(
-    cookieSession({
-        secret: `I'm always angry.`,
-        maxAge: 1000 * 60 * 60 * 24 * 14
-    })
+  cookieSession({
+    secret: `I'm always angry.`,
+    maxAge: 1000 * 60 * 60 * 24 * 14
+  })
 );
 
 app.use(csurf());
 
 app.use(function(req, res, next) {
-    res.set("x-frame-options", "DENY");
-    res.locals.csrfToken = req.csrfToken();
-    next();
+  res.set("x-frame-options", "DENY");
+  res.locals.csrfToken = req.csrfToken();
+  next();
 });
 
 //
 app.use(function(req, res, next) {
-    if (!req.session.userId && req.url != "/register" && req.url != "/login") {
-        res.redirect("/register");
-    } else {
-        next();
-    }
+  if (!req.session.userId && req.url != "/register" && req.url != "/login") {
+    res.redirect("/register");
+  } else {
+    next();
+  }
 });
 
 app.get("/", (req, res) => {
-    res.redirect("/register");
+  res.redirect("/register");
 });
 
 app.get("/petition", requireNoSignature, (req, res) => {
-    if (req.session.sigId) {
-        res.redirect("/thanks");
-    } else {
-        res.render("petition", {
-            layout: "main"
-        });
-    }
+  if (req.session.sigId) {
+    res.redirect("/thanks");
+  } else {
+    res.render("petition", {
+      layout: "main"
+    });
+  }
 });
 
 app.get("/register", requireLoggedOutUser, (req, res) => {
-    res.render("registration", {
-        layout: "main"
-    });
+  res.render("registration", {
+    layout: "main"
+  });
 });
 
 app.post("/register", (req, res) => {
-    if (
-        req.body.first &&
-        req.body.second &&
-        req.body.email &&
-        req.body.password
-    ) {
-        hash(req.body.password).then(hash => {
-            addUser(req.body.first, req.body.second, req.body.email, hash)
-                .then(results => {
-                    req.session.userId = results.rows[0].id;
-                    req.session.first = results.rows[0].first;
-                    req.session.last = results.rows[0].last;
-                    req.session.email = results.rows[0].email;
-                    // req.session.userId = results.rows[0].id;
-                    res.redirect("/profile");
-                })
-                .catch(err => {
-                    console.log("error in registration", err);
-                });
+  if (
+    req.body.first &&
+    req.body.second &&
+    req.body.email &&
+    req.body.password
+  ) {
+    hash(req.body.password).then(hash => {
+      addUser(req.body.first, req.body.second, req.body.email, hash)
+        .then(results => {
+          req.session.userId = results.rows[0].id;
+          req.session.first = results.rows[0].first;
+          req.session.last = results.rows[0].last;
+          req.session.email = results.rows[0].email;
+          // req.session.userId = results.rows[0].id;
+          res.redirect("/profile");
+        })
+        .catch(err => {
+          console.log("error in registration", err);
         });
-    } else {
-        res.render("registration", {
-            layout: "main",
-            errorMessage: "you must fill out all fields to register"
-        });
-    }
+    });
+  } else {
+    res.render("registration", {
+      layout: "main",
+      errorMessage: "you must fill out all fields to register"
+    });
+  }
 });
 
 app.get("/profile", (req, res) => {
-    res.render("profile", {
-        layout: "main"
-    });
+  res.render("profile", {
+    layout: "main"
+  });
 });
 
 app.post("/profile", (req, res) => {
-    var userUrl = req.body.homepage;
-    console.log("userUrl", userUrl);
-    if (!req.body.city && !req.body.url && !req.body.age && userUrl) {
-        res.redirect("/petition");
-    }
-    if (
-        userUrl.startsWith("http://") ||
-        userUrl.startsWith("https://") ||
-        userUrl.startsWith("//")
-    ) {
-        userUrl = req.body.homepage;
-        console.log("req.body.homepage", req.body.homepage);
-    } else {
-        userUrl = "http://" + userUrl;
-        console.log("new url", userUrl);
-        console.log("url not valid");
-    }
-    newProfile(req.body.age, req.body.city, userUrl, req.session.userId)
-        .then(results => {
-            console.log("results in post profile", results);
-            res.redirect("/petition");
-        })
-        .catch(err => {
-            console.log("err in post profile", err);
-            res.render("profile", {
-                layout: "main",
-                errorMessage:
-                    "The web site has to starti with http:// or https://"
-            });
-        });
+  var userUrl = req.body.homepage;
+  if (!req.body.city && !req.body.url && !req.body.age && userUrl) {
+    res.redirect("/petition");
+  }
+  if (
+    userUrl.startsWith("http://") ||
+    userUrl.startsWith("https://") ||
+    userUrl.startsWith("//")
+  ) {
+    userUrl = req.body.homepage;
+  } else {
+    userUrl = "http://" + userUrl;
+  }
+  newProfile(req.body.age, req.body.city, userUrl, req.session.userId)
+    .then(results => {
+      console.log("results in post profile", results);
+      res.redirect("/petition");
+    })
+    .catch(err => {
+      console.log("err in post profile", err);
+      res.render("profile", {
+        layout: "main",
+        errorMessage: "The web site has to starti with http:// or https://"
+      });
+    });
 });
 
 app.get("/profile/edit", (req, res) => {
-    getAllUserData(req.session.userId).then(results => {
-        let allData = results.rows;
-        res.render("edit", {
-            layout: "main",
-            allData
-        });
+  getAllUserData(req.session.userId).then(results => {
+    let allData = results.rows;
+    res.render("edit", {
+      layout: "main",
+      allData
     });
+  });
 });
 
 app.post("/profile/edit", (req, res) => {
-    let first = req.body.first;
-    let last = req.body.second;
-    let email = req.body.email;
-    let password = req.body.password;
-    let url = req.body.homepage;
-    let age = req.body.age;
-    let city = req.body.city;
-    console.log("req.body.password", req.body.password);
-    if (!password) {
-        console.log("no password to be updated");
-        updateProfile(age, city, url, req.session.userId)
-            .then(results => {
-                console.log("results.rows in /profile/edit", results.rows);
-                updateUsersTable(req.session.userId, first, last, email);
-                res.redirect("/thanks");
-            })
-            .catch(err => console.log("err in profile edit post", err));
-    } else {
-        updateProfile(age, city, url, req.session.userId).then(() => {
-            hash(req.body.password)
-                .then(hash => {
-                    console.log("hash", hash);
-                    updateUsersTableWithPw(
-                        req.session.userId,
-                        first,
-                        last,
-                        email,
-                        hash
-                    );
-                })
-                .catch(err => console.log("err in profile edit post", err));
-        });
+  let first = req.body.first;
+  let last = req.body.second;
+  let email = req.body.email;
+  let password = req.body.password;
+  let url = req.body.homepage;
+  let age = req.body.age;
+  let city = req.body.city;
+  console.log("req.body.password", req.body.password);
+  if (!password) {
+    updateProfile(age, city, url, req.session.userId)
+      .then(results => {
+        console.log("results.rows in /profile/edit", results.rows);
+        updateUsersTable(req.session.userId, first, last, email);
         res.redirect("/thanks");
-    }
+      })
+      .catch(err => console.log("err in profile edit post", err));
+  } else {
+    updateProfile(age, city, url, req.session.userId).then(() => {
+      hash(req.body.password)
+        .then(hash => {
+          updateUsersTableWithPw(req.session.userId, first, last, email, hash);
+        })
+        .catch(err => console.log("err in profile edit post", err));
+    });
+    res.redirect("/thanks");
+  }
 });
 
 app.get("/login", requireLoggedOutUser, (req, res) => {
-    res.render("login", {
-        layout: "main"
-    });
+  res.render("login", {
+    layout: "main"
+  });
 });
 
 app.post("/login", requireLoggedOutUser, (req, res) => {
-    console.log("req.body.email", req.body.email);
-    getDataFromEmail(req.body.email)
-        .then(results => {
-            req.session.sigId = results.rows[0].id;
-            if (results.rows[0].email == req.body.email) {
-                console.log("existing mail in db");
-                // hash(req.body.password).then(hashedPw => {
-                compare(req.body.password, results.rows[0].password)
-                    .then(matchValue => {
-                        console.log("match value: ", matchValue);
-                        if (matchValue == true) {
-                            req.session.userId = results.rows[0].user_id;
-                            res.redirect("/petition");
-                        } else {
-                            res.render("login", {
-                                layout: "main",
-                                errorMessage: "wrong email or password"
-                            });
-                        }
-                    })
-                    .catch(err => console.log("err in post login", err));
-            }
-        })
-        .catch(err => {
-            console.log("err in post login", err);
-            res.render("login", {
+  getDataFromEmail(req.body.email)
+    .then(results => {
+      req.session.sigId = results.rows[0].id;
+      if (results.rows[0].email == req.body.email) {
+        compare(req.body.password, results.rows[0].password)
+          .then(matchValue => {
+            if (matchValue == true) {
+              req.session.userId = results.rows[0].user_id;
+              res.redirect("/petition");
+            } else {
+              res.render("login", {
                 layout: "main",
                 errorMessage: "wrong email or password"
-            });
-        });
+              });
+            }
+          })
+          .catch(err => console.log("err in post login", err));
+      }
+    })
+    .catch(err => {
+      console.log("err in post login", err);
+      res.render("login", {
+        layout: "main",
+        errorMessage: "wrong email or password"
+      });
+    });
 });
 
 app.get("/thanks", requireSignature, (req, res) => {
-    let id = req.session.userId;
-    getSigImg(id).then(results => {
-        let nameAndSig = results.rows[0];
-        res.render("thanks", {
-            layout: "main",
-            nameAndSig: nameAndSig
-        });
+  let id = req.session.userId;
+  getSigImg(id).then(results => {
+    let nameAndSig = results.rows[0];
+    res.render("thanks", {
+      layout: "main",
+      nameAndSig: nameAndSig
     });
+  });
 });
 
 app.get("/signers", requireSignature, (req, res) => {
-    getSigners().then(results => {
-        let subscribers = results.rows;
-        res.render("signers", {
-            layout: "main",
-            subscribers
-        });
+  getSigners().then(results => {
+    let subscribers = results.rows;
+    res.render("signers", {
+      layout: "main",
+      subscribers
     });
+  });
 });
 
 app.get("/signers/:city", requireSignature, (req, res) => {
-    console.log("req.params.city", req.params.city);
-    getSignerFromCity(req.params.city).then(results => {
-        let subscribers = results.rows;
-        res.render("signers", {
-            layout: "main",
-            subscribers
-        });
+  console.log("req.params.city", req.params.city);
+  getSignerFromCity(req.params.city).then(results => {
+    let subscribers = results.rows;
+    res.render("signers", {
+      layout: "main",
+      subscribers
     });
+  });
 });
 
 app.post("/thanks", (req, res) => {
-    res.redirect("/signers");
+  res.redirect("/signers");
 });
 
 app.post("/petition", (req, res) => {
-    const { userId } = req.session;
-    addSigner(req.body.sig, userId)
-        .then(results => {
-            console.log("req.body.sig", req.body.sig);
-            req.session.sigId = results.rows[0].id;
-            res.redirect("/thanks");
-        })
-        .catch(err => console.log("error in post petition", err));
+  const { userId } = req.session;
+  addSigner(req.body.sig, userId)
+    .then(results => {
+      req.session.sigId = results.rows[0].id;
+      res.redirect("/thanks");
+    })
+    .catch(err => console.log("error in post petition", err));
 });
 
 app.get("/logout", (req, res) => {
-    req.session = null;
-    res.redirect("login");
+  req.session = null;
+  res.redirect("login");
 });
 
 app.get("/delete", (req, res) => {
-    console.log("req.session.sigId", req.session.sigId);
-    deleteSig(req.session.userId);
-    req.session.sigId = null;
-    res.redirect("/petition");
+  deleteSig(req.session.userId);
+  req.session.sigId = null;
+  res.redirect("/petition");
 });
 
 if (require.main === module) {
-    app.listen(process.env.PORT || 8080, () =>
-        console.log("Petition server is listening")
-    );
+  app.listen(process.env.PORT || 8080, () =>
+    console.log("Petition server is listening")
+  );
 }
