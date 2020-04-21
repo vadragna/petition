066e577633770c9bac73dcef7c40f442fff180e2
Valerio Adragna <valerioadragna89@hotmail.it>
2020-04-21 16:28:20 +0200

    read.me added

---
+++ README.md
@@ -0,0 +1,22 @@
+# Petition Project
+
+![](public/screenshot.png)
+
+## Summary
+
+This web petition application made during my Fullstack Web Dev intensive course at SPICED academy. An user should register to the plattform add their info, sign for the petition, change their signature.
+
+## Technologies
+
+- JavaScript
+- Node.js
+- Express.js
+- SQL
+
+## Feautures
+
+- Registration
+- Log in
+- Sign (Canvas)
+- Delete their signature
+- See other users
--- public/script.js
+++ public/script.js
@@ -6,40 +6,32 @@ var ctx = canvas[0].getContext("2d");
 ctx.lineCap = "round";
 
 ctx.beginPath();
-canvas.on("mousedown", event => {
-    var draw = true;
+canvas.on("mousedown", (event) => {
+  var draw = true;
+  if (draw) {
+    ctx.moveTo(event.offsetX, event.offsetY);
+  }
+  canvas.on("mousemove", (e) => {
     if (draw) {
-        ctx.moveTo(event.offsetX, event.offsetY);
+      ctx.lineTo(e.offsetX, e.offsetY);
+      ctx.stroke();
     }
-    canvas.on("mousemove", e => {
-        if (draw) {
-            ctx.lineTo(e.offsetX, e.offsetY);
-            ctx.stroke();
-        }
-    });
-    canvas.on("mouseup", () => {
-        console.log("sig", sig);
-        draw = false;
-        $("input[name='sig']").val(canvas[0].toDataURL());
-    });
+  });
+  canvas.on("mouseup", () => {
+    draw = false;
+    $("input[name='sig']").val(canvas[0].toDataURL());
+  });
 });
 
 $("#submit").on("click", () => {
-    console.log("clicked");
-    console.log("sig", sig);
-    console.log('$("#message").html()', $("#message").html());
-    var sig = $("input#sig").html();
-    if (sig != "") {
-        var sigCode = $("input[name='sig']").val(canvas[0].toDataURL());
-        var first = $("#first").val();
-        var last = sigCode[0].value;
-        console.log(sig, "sig", "first", first, "last", last);
-        console.log("clicked on submit button");
-    } else {
-        if ($("#message").html().length == 0) {
-            $("#message").append(
-                "<h5>You must sign to send your subscription!</h5>"
-            );
-        }
+  var sig = $("input#sig").html();
+  if (sig != "") {
+    var sigCode = $("input[name='sig']").val(canvas[0].toDataURL());
+    var first = $("#first").val();
+    var last = sigCode[0].value;
+  } else {
+    if ($("#message").html().length == 0) {
+      $("#message").append("<h5>You must sign to send your subscription!</h5>");
     }
+  }
 });
